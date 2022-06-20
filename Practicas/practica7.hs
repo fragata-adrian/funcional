d7639b3f6d0bde06b3832c66d10ba5ad3e2b43f6
fragata-adrian <fragataadrian99@gmail.com>
2022-06-20 16:00:18 -0300

    añadido practica 8

--- Practicas/practica7.hs
+++ Practicas/practica7.hs
@@ -4,7 +4,34 @@ esqFoldr r z = g
           g ( x : xs ) = r x ( g xs )
 
 
-sumatoria = esqFoldr (+) 0
+esqMap :: ( a -> b ) -> [ a ] -> [ b ]
+esqMap f = g
+    where g [] = []
+          g ( x : xs ) = f x : g xs
 
 
-esqMap f = esqFoldr f [] 
+esqFilter :: ( a -> Bool ) -> [ a ] -> [ a ]
+esqFilter f = g
+    where g [] = []
+          g ( x : xs ) | f x = x : g xs
+                       | otherwise = g xs
+
+----------------------------------------
+-------- esqMap con foldr
+----------------------------------------
+esqMap' f = esqFoldr (\x rc -> f x : rc) []
+
+-- Usando composicion:
+esqMap'' f = esqFoldr ((:).f) []
+
+----------------------------------------
+-------- esqFilter con foldr
+----------------------------------------
+
+-- Defino la funcion agregarSi para suprimir las guardas ya que no tengo
+-- forma de utilizar guardas en foldr
+agregarSi :: (a -> Bool) -> a -> [a] -> [a]
+agregarSi f x xs | f x = x : xs
+                 | otherwise = xs
+
+esqFilter' f = esqFoldr (\x rc -> agregarSi f x rc ) []
---
+++ Practicas/practica8.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE InstanceSigs #-}
+{-
+    1) Definir un tipo enumerado Color para representar los colores
+    negro, azul, rojo y blanco. Implementar la función
+    esClaro :: Color -> Bool que devuelve True si el color es rojo
+    o blanco, y False en caso contrario. Resolverlo con pattern
+    matching y con la igualdad derivada de Eq.
+-}
+
+-------------------------------------------
+----- Solucion con pattern matching -------
+-------------------------------------------
+
+data Color = Negro | Azul | Rojo | Blanco
+
+instance Eq Color where 
+    (==) :: Color -> Color -> Bool
+    Negro == Negro = True
+    Azul == Azul = True
+    Rojo == Rojo = True
+    Blanco == Blanco = True
+    _ == _ = False
+
+------------------------------------------
+----- Solucion con derivada de Eq --------
+------------------------------------------
+
+--data Color = Negro | Azul | Rojo | Blanco deriving Eq
+
+------------------------------------------
+----------- Funcion esClaro --------------
+------------------------------------------
+
+esClaro :: Color -> Bool
+esClaro c = c == Rojo || c == Blanco
+
+
+{-
+    2) Definir la función producto :: Nat -> Nat -> Nat.  
+-}
+
+-- data Nat = Zero | Succ Nat
+
+-- producto :: Nat -> Nat -> Nat
+-- producto _ Zero = Zero
+-- producto n (Succ m) = Succ * (producto n m) 
+
+{-
+    3) Definir la función size :: Arbol a -> Int que devuelve la
+    cantidad total de elementos en el árbol.
+-}
+
+data Arbol a = Hoja a | Nodo a (Arbol a) (Arbol a)
+
+arbol = Nodo 1 ( Nodo 2 ( Hoja 3) ( Hoja 5) ) ( Hoja 7)
+
+size :: Arbol a -> Int
+size (Hoja _) = 1
+size (Nodo _ izq der) = 1 + size izq + size der
